f3bb863d99d0e0b406b7d9b8f392b509e1a77854
rinkuberde <rinkuberde@gmail.com>
2023-03-14 18:23:53 +0530

    On Document Load Set Event Listeners

---
+++ js/new_payroll_form.js
@@ -0,0 +1,25 @@
+/*UC 2 – On Document Load Set Event Listeners*/
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name=name.value;;
+            textError.textContent = "";
+        }
+        catch (e) {
+            textError.textContent = e;
+        }
+    });
+    /** set event listener on salary range*/
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
+}); 
